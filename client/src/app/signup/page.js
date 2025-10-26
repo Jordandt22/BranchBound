9d06bd5539c9d41de1f6bdd96fbc192ddff7977e
Jordandt22 <jordandt123411@gmail.com>
2025-10-25 20:08:32 -0700

    (Client): Added Placeholder UI for Sign Up Page.

--- client/src/app/signup/page.js
+++ client/src/app/signup/page.js
@@ -1,3 +1,15 @@
+import MobileAuthForm from "@/components/auth/MobileAuthForm";
+import DesktopAuthForm from "@/components/auth/DesktopAuthForm";
+
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <MobileAuthForm />
+      <DesktopAuthForm
+        title="Sign up"
+        subTitle="Welcome to BranchBound"
+        description="Experience interactive stories where your choices shape the narrative. Choose your path and create unique adventures."
+      />
+    </div>
+  );
 }
